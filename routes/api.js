1d959a614364fadac3de536bdf7e9a510fbe1546
Rishav Rakesh Thapliyal <rishabhthapliyal03@gmail.com>
2022-03-09 13:26:02 +0530

    Made a new shpw page for the series

--- routes/api.js
+++ routes/api.js
@@ -13,21 +13,27 @@ const YOUTUBE_URL = "https://www.youtube.com/embed/";
 const VIDEO_URL = "/videos";
 
 router.get('/', (req, res) => {
+    try{
     getData();
     async function getData() {
         const result = await axios.get(API_URL);
         const tv_show = await axios.get(TV_API);
-        console.log(tv_show.data)
+        console.log(tv_show.data.results[0])
         res.render('layouts/api', { data: result.data.results, image: IMAGE_URL, tv: tv_show.data.results, name: req.user });
+    }
+    }
+    catch(err){
+        console.log(err)
     }
 });
 
-router.get('/:id/show', (req, res) => {
+router.get('/:id/movie/show', (req, res) => {
     getData();
     async function getData() {
         try {
             const result = await axios.get(DATA + req.params.id + "?" + API_KEY);
             const video = await axios.get(`${DATA}${req.params.id}${VIDEO_URL}?${API_KEY}`);
+
             const query = "select * from reviews,users where reviews.user_id=users.id and reviews.movie_id=?"
             db.query(query,[req.params.id],(err, queryRes) => {
                 if (err) throw err;
@@ -47,7 +53,35 @@ router.get('/:id/show', (req, res) => {
         } catch (e) {
             console.log(e)
         }
-        let no_review = 0;
+    }
+})
+
+router.get('/:id/series/show',(req,res)=>{
+    try {
+       getData();
+        async function getData(){
+            const tv_show=await axios.get(TV_API);
+            for(const details of tv_show.data.results){
+                if(details.id==req.params.id){
+                    const query = "select * from reviews,users where reviews.user_id=users.id and reviews.movie_id=?"
+                    db.query(query,[req.params.id],(err, queryRes) => {
+                        if (err) throw err;
+                        res.render("layouts/api_series_show",
+                            {
+                                data: details,
+                                image: IMAGE_URL,
+                                name: req.user,
+                                youtube: YOUTUBE_URL,
+                                user_id: req.user,
+                                review:queryRes[0]
+                            }
+                        );
+                    })
+                }
+            }
+     } 
+    } catch (error) {
+        console.log(error);
     }
 })
 
